92f7290738fc66a1301758f1484406a1d075eb66
Mar <mar@coconauts.net>
2015-10-04 21:12:33 +0100

    Sample makefile to customize

--- make.sh
+++ make.sh
@@ -1,10 +1,10 @@
-    #!/bin/bash
+#!/bin/bash
     
-    SRC_DIR=watch
-    
-    cp Makefile $SRC_DIR
-    cd $SRC_DIR
-    make $1
-    rm Makefile
-    cd ..
+SRC_DIR=watch
+
+cp Makefile $SRC_DIR
+cd $SRC_DIR
+make $1
+rm Makefile
+cd ..
 
